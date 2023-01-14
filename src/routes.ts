ba91d4969f154614437b3b56772a99eb7d34b0ae
João Mário Silva Nascimento <marioartec39@gmail.com>
2023-01-14 15:56:44 -0300

    Update routes.ts

--- src/routes.ts
+++ src/routes.ts
@@ -13,7 +13,7 @@ async function Lily() {
         const { Client, LocalAuth } = require('whatsapp-web.js');
         const client = new Client({
             authStrategy: new LocalAuth(),
-            puppeteer: { headless: true, args: ['--no-sandbox'], }
+            puppeteer: { headless: true, args: ['--no-sandbox', '--disable-setuid-sandbox'] }
 
         });
         
